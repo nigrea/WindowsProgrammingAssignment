f9718b024ea118a2b4214484142cbc0c7807c146
Nigrea <nigrea@gmail.com>
2014-10-07 11:19:56 +0200

    Restructuring

--- WindowsProgrammingAssignment/View/MainWindow.xaml.cs
+++ WindowsProgrammingAssignment/View/MainWindow.xaml.cs
@@ -25,7 +25,7 @@ namespace WindowsProgrammingAssignment
         public MainWindow()
         {
             InitializeComponent();
-
+            
 
 
         }
---
+++ WindowsProgrammingAssignment/ViewModel/MainViewModel.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsProgrammingAssignment.ViewModel
+{
+    class MainViewModel
+    {
+
+       // public ICommand UndoCommand { get; private set; }
+
+
+    }
+}
